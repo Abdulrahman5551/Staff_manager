4eeae7504be243077500b9423811fba4fe5d3c43
Abdulrahman5551 <AbdulrahmanBinSaad-55@outlook.sa>
2024-04-17 02:29:41 +0300

    Design page dashboard 006

--- src/App_Employee/views.py
+++ src/App_Employee/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'App_Employee\index.html')
+    context = {
+        'title': 'Index Page',
+    }
+    return render(request, 'App_Employee\index.html', context)
+
+def dashboard(request):
+    context = {
+        'title': 'Dashboard Page',
+    }
+    return render(request, 'App_Employee\dashboard.html', context)
