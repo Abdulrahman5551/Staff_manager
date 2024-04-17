877d393357dc4e7b794549af143855188fdd69e6
Abdulrahman5551 <AbdulrahmanBinSaad-55@outlook.sa>
2024-04-17 07:49:26 +0300

    Design page for compensation & department and edit tables 008

--- src/App_Employee/urls.py
+++ src/App_Employee/urls.py
@@ -4,4 +4,12 @@ from . import views
 urlpatterns = [
    path('', views.index, name="index"),
    path('dashboard/', views.dashboard, name="dashboard"),
+   path('create-employee/', views.add_employee, name="create-employee"),
+
+
+   path('department/', views.departments, name="department"),
+   path('create-department/', views.add_department, name="create-department"),
+
+   path('compensation/', views.compensations, name="compensations"),
+   path('create-compensation/', views.add_compensation, name="create-compensation"),
 ]
--- src/App_Employee/views.py
+++ src/App_Employee/views.py
@@ -13,3 +13,34 @@ def dashboard(request):
         'title': 'Dashboard Page',
     }
     return render(request, 'App_Employee\dashboard.html', context)
+
+def add_employee(request):
+    context = {
+        'title': 'Add Employee',
+    }
+    return render(request, 'App_Employee\\add_employee.html', context)
+
+def departments(request):
+    context = {
+        'title': 'Departments Page',
+    }
+    return render(request, 'App_Employee\departments.html', context)
+
+def add_department(request):
+    context = {
+        'title': 'Add Department',
+    }
+    return render(request, 'App_Employee\\add_department.html', context)
+
+
+def compensations(request):
+    context = {
+        'title': 'Compensation Page',
+    }
+    return render(request, 'App_Employee\compensations.html', context)
+
+def add_compensation(request):
+    context = {
+        'title': 'Add Compensation',
+    }
+    return render(request, 'App_Employee\\add_compensations.html', context)
